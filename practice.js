1713f8a9e21addc155386972165cb6b668f1cf4a
thankyou1113 <kmalan9213@gmail.com>
2021-04-12 22:12:17 +0900

    ASYNC 연습

--- practice.js
+++ practice.js
@@ -1,27 +1,23 @@
-/*
-  Q5) 숫자 n을 입력받아 역삼각형 모양의 문자를 "출력"하는 printInvertedTriangle 함수를 완성해주세요. (30점)
-  제한사항: n은 2이상, 10 이하의 양수입니다.
-
-  n = 2일 때 아래와 같이 출력
-  * *
-   *
-
-  n = 10일 때 아래와 같이 출력
-  * * * * * * * * * * 
-   * * * * * * * * *
-    * * * * * * * *
-     * * * * * * *
-      * * * * * *
-       * * * * *
-        * * * *
-         * * *
-          * *
-           * 
-*/
-
-function printInvertedTriangle(n) {
-    return (n > 0) ? '*'.repeat(n) + '\n' + printInvertedTriangle(n-1) : ''
+function p(ms) {
+  return new Promise((resolve, reject) => {
+//p 함수가 new Promise를 return하고 있다.
+    setTimeout(() => {
+      // resolve(ms);
+      reject(new Errors('reason'));
+    }, ms);
+  });
 }
 
-console.log(printInvertedTriangle(2));
-console.log(printInvertedTriangle(10));
+async function asyncP() {
+  const ms = await p(1000);
+  return 'Dalso: ' + ms;
+}
+
+(async function dalso() {
+  try {
+  const name = await asyncP();
+  console.log(name);
+} catch (error) {
+console.log(error);
+}
+})();
