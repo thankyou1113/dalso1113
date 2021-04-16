bf3e11d2fa23b7448a746139b1ae34af5cc1f4b5
thankyou1113 <kmalan9213@gmail.com>
2021-04-16 21:47:41 +0900

    splice 활용 예제

--- practice.js
+++ practice.js
@@ -1,38 +1,8 @@
-const numbers = {
-  _a: 1,
-  _b: 2,
-  sum: 3,
-  calculate() {
-      console.log('calculate');
-      this.sum = this._a + this._b;
-   },
-  get a() {
-      return this._a;
-  },
-  get b() {
-      return this._b;
-  },
-  set a(value) {
-// set 함수 때는 값을 무조건 넣어 주어야 한다.
-      this._a = value;
-// this._a를 value로 바꿨다.
-      this.calculate();
-// sum 값을 업데이트해주었다.
-  },
-  set b(value) {
-      this._b = value;
-      this.calculate();
-  }
-};
-
-console.log(numbers.sum);
-// 여기까지는 출력값이 3이다.
-numbers.a = 5;
-// 여기까지는 calculate이라고 출력값이 뜬다.
-/* 5가 set a(value)의 value로 들어가서 this._a를 갖게 되고, 그러면서 _a: 1한테 설정이 된다.
-그러고 나서 this.calculate()가 실행되면서 claculate()이 실행되면 this.sum = this._a + this._b;이 연산된 값(sum: 3)으로 바뀌게 된다. */
-console.log(numbers.sum);
-numbers.a = 6;
-numbers.b = 7;
-// 값이 바뀔 때마다 sum 출력값도 달라진다.
-console.log(numbers.sum);
+const numbers = [10, 20, 30, 40];
+const index = numbers.indexOf(30);
+console.log(index);
+// 여기서 출력값은 2. 여기서 우리는 index === 2인 것을 slice를 이용하여 제거할 것이다.
+numbers.splice(index, 1);
+// index부터 시작해서 한 개 지우겠다는 뜻.
+// 파라미터로 1 대신 2를 넣으면 30, 40이 제거된다.
+console.log(numbers);
