41a37a1950d6c01cffbdd01417ec17d6310cf8aa
thankyou1113 <kmalan9213@gmail.com>
2021-04-19 22:22:01 +0900

    forEach 연습

--- practice.js
+++ practice.js
@@ -1,25 +1,5 @@
-function increaseAndPrint(n) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const value = n + 1;
-            if (value === 5) {
-                const error = new Error();
-                error.name = 'ValueIsFiveError';
-                reject(error);
-                return;
-            }
-            console.log(value);
-            resolve(value);
-        }, 1000);
-    });
-}
+const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
 
-increaseAndPrint(0)
-    .then(increaseAndPrint)
-    .then(increaseAndPrint)
-    .then(increaseAndPrint)
-    .then(increaseAndPrint)
-    .then(increaseAndPrint)
-    .catch(e => {
-        console.error(e);
-    });
+superheroes.forEach(hero => {
+    console.log(hero);
+});
