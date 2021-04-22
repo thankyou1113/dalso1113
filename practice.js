b4eee9edcb4f744527c6c176c7b9971bf65f244a
thankyou1113 <kmalan9213@gmail.com>
2021-04-22 19:00:43 +0900

    truthy & falsy 연습

--- practice.js
+++ practice.js
@@ -1,27 +1,10 @@
-function Animal(type, name, sound) {
-  this.type = type;
-  this.name = name;
-  this.sound = sound;
+const array = [];
+let text = ' ';
+
+if (array.length === 0) {
+    text = '배열이 비어 있다.';
+} else {
+    text = '배열이 비어 있지 않다.';
 }
 
-
-Animal.prototype.say = function () {
-  console.log(this.sound);
-}
-
-
-Animal.prototype.sharedValue = 1;
-
-const cat = new Animal('고양이', '소다', '야옹');
-const dog = new Animal('강아지', '바둑이', '멍멍');
-
-
-cat.sharedValue
-dog.sharedValue
-
-
-cat.say = say;
-dog.say = say;
-
-cat.say();
-dog.say();
+console.log(text);
