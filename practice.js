3a1226fe7583e9de87c53fe81321d3bd83dbea65
thankyou1113 <kmalan9213@gmail.com>
2021-04-27 19:50:14 +0900

    화살표 함수 연습

--- practice.js
+++ practice.js
@@ -1,8 +1,4 @@
-function isAnimal(text) {
-    const animals = ['개', '다람쥐', '고양이', '호랑이'];
-    return animals.includes(text);
-// 배열[]안에 text가 존재하면 true, 존재하지 않으면 false
-}
+const isAnimal = (text) => ['개', '다람쥐', '고양이', '호랑이'].includes(text);
 
 console.log(isAnimal('다람쥐'));
 console.log(isAnimal('시계'));
