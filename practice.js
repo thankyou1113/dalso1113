ecedf3de09c39b03255c38225b8617493170082c
thankyou1113 <kmalan9213@gmail.com>
2021-05-04 21:08:48 +0900

    연습

--- practice.js
+++ practice.js
@@ -1,25 +1,4 @@
-function makeSound(animal) {
-    const tasks = {
-        개: () => {
-            console.log('멍멍!');
-        },
-        고양이() {
-            console.log('야옹~');
-        },
-        쥐: function() {
-            console.log('찍찍');
-        }
-// 위의 세 개의 객체들은 모두 같은 형식이다. 두 번째 객체가 가장 깔끔한 코드이다.
-    }
-
-    const task = tasks[animal];
-    if (!task) {
-        console.log('...?');
-        return;
-    }
-    task();
-}
-
-
-makeSound('개');
-makeSound('인간');
+const object = { a: 1, b: 2 };
+const { a, b } = object;
+console.log(a);
+console.log(b);
