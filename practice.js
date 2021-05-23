9c8b22bb680af7e594e76f28cbf82b9e720827d4
thankyou1113 <kmalan9213@gmail.com>
2021-05-23 20:14:58 +0900

    같은 코드

--- practice.js
+++ practice.js
@@ -1,6 +1,5 @@
 const animals = ['고양이', '개', '호랑이'];
-const anotherAnimals = [...animals, '쥐'];
-// 기존의 배열(animals)은 건드리지 않으면서 anotherAnimals에 기존의 배열을 그대로 추가할 수 있다.
+const anotherAnimals = animals.concat('쥐');
 
 console.log(animals);
 console.log(anotherAnimals);
