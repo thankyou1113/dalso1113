d0b3be660ead6a6f192afd59ff77f39b697becf7
thankyou1113 <kmalan9213@gmail.com>
2021-05-25 20:28:28 +0900

    rest 연산자

--- practice.js
+++ practice.js
@@ -1,4 +1,14 @@
-const numbers = [1, 2, 3, 4, 5];
-const spreadNumbers = [...numbers, 1000, ...numbers];
+const grayCuteDalso = {
+    name: '달소',
+    attribute: '큐트',
+    color: '회색'
+};
 
-console.log(spreadNumbers);
+const { color, ...rest } = grayCuteDalso;
+// 객체 비구조화 할당을 하면서 그 안에 rest 연산자를 사용했다.
+console.log(color);
+console.log(rest);
+// color 객체를 제외한 다른 객체와 값들(name, attribute)이 rest 안에 들어와서, 그 rest값들이 출력된다.
+
+const { attribute, ...dalso } = rest;
+console.log(dalso);
