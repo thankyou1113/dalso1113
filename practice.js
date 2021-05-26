5b2e805aa841f1fe405d444250bb18ddbc07e593
thankyou1113 <kmalan9213@gmail.com>
2021-05-26 20:55:50 +0900

    배열에서 rest 쓰기

--- practice.js
+++ practice.js
@@ -1,14 +1,6 @@
-const grayCuteDalso = {
-    name: '달소',
-    attribute: '큐트',
-    color: '회색'
-};
+const numbers = [0, 1, 2, 3, 4, 5, 6];
 
-const { color, ...rest } = grayCuteDalso;
-// 객체 비구조화 할당을 하면서 그 안에 rest 연산자를 사용했다.
-console.log(color);
+const [one, two, ...rest] = numbers;
+console.log(one);
+console.log(two);
 console.log(rest);
-// color 객체를 제외한 다른 객체와 값들(name, attribute)이 rest 안에 들어와서, 그 rest값들이 출력된다.
-
-const { attribute, ...dalso } = rest;
-console.log(dalso);
