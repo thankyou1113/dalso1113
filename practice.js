a90344db5a3b7d0882a91ee8baec183b84d793c4
thankyou1113 <kmalan9213@gmail.com>
2021-05-27 20:17:11 +0900

    rest 연산자 오류

--- practice.js
+++ practice.js
@@ -1,6 +1,4 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6];
 
-const [one, two, ...rest] = numbers;
-console.log(one);
-console.log(two);
-console.log(rest);
+const [...rest, last] = numbers;
+console.log(last);
