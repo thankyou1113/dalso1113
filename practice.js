bc7616265a886520d36bd46b5224681fdb45e51d
thankyou1113 <kmalan9213@gmail.com>
2021-05-31 21:26:45 +0900

    파라미터와 인자의 차이점

--- practice.js
+++ practice.js
@@ -1,6 +1,13 @@
 function sum(...rest) {
     return rest.reduce((acc, current) => acc + current, 0);
-// 기본값은 0으로 시작해서, 이 0이 acc값이 되고, current는 1,2,3,4,5,6,7,8이 된다. 그래서 acc + current가 순차적으로 실행된다.
 }
 
 console.log(sum(1, 2, 3, 4, 5, 6, 7, 8));
+
+function subtract(x, y) {
+    return x - y;
+}
+// x와 y는 파라미터이다.
+
+const result = subtract(1, 2);
+// 1과 2는 인자이다.
